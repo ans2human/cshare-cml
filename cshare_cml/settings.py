07b9fb97c0f4deada08be18f9c5652dba8262f37
Anshuman Singh <anshuman@Anshumans-MacBook-Air.local>
2019-12-02 17:17:28 +0530

    settings updated

--- cshare_cml/settings.py
+++ cshare_cml/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["ans2human.pythonanywhere.com", "localhost"]
 
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
+]
 
 # Application definition
 

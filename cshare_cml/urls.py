03f1c3c230a76b940456de04a500034da523310f
Anshuman Singh <anshuman@Anshumans-MacBook-Air.local>
2019-12-02 16:38:42 +0530

    redirectview added

--- cshare_cml/urls.py
+++ cshare_cml/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('core.urls')),
+    path('', RedirectView.as_view(url = '/api/v1/', permanent = True)),
 ]
